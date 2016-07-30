4ec625ac5b78b51ddf294a7ebca3d9fc50a7b4ea
77377xiang <xiang792178903@sina.com>
2016-07-30 22:19:09 +0800

    百度定位

---
+++ MapProject/app/src/main/java/com/xiang/map/mapproject/MapLoaction.java
@@ -0,0 +1,88 @@
+package com.xiang.map.mapproject;
+
+/**
+ * Created by Administrator on 2016/7/17.
+ */
+public class MapLoaction {
+    public String country;
+    public String countryCode;
+    public String province;
+    public String city;
+    public String cityCode;
+    public String district;
+    public String street;
+    public String streetNumber;
+    public String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+}
